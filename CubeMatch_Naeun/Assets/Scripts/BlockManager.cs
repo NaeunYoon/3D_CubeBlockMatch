204b7da3f91154243093c65811480b47b8548ee0
NaeunYoon <whalebaby@naver.com>
2023-03-09 00:09:33 +0900

    Add_Particle

---
+++ CubeMatch_Naeun/Assets/Scripts/BlockManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BlockManager : MonoBehaviour
+{
+    //블럭에 부여된 번호를 표시
+    [SerializeField]
+    private TextMesh _NumberText;
+
+    //테두리 표시
+    [SerializeField]
+    private GameObject _Edge;
+
+    [SerializeField]
+    private ParticleSystem _Particle;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
